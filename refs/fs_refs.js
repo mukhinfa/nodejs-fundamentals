4b1935ad4a5cf1f3dda47d8f2c5b14b009663cd0
Fedor M <muhinfa@gmail.com>
2022-05-15 22:14:10 +0300

    fs tutorial

---
+++ refs/fs_refs.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+// fs.mkdir(path.join(__dirname, 'notes'), (err) => {
+//     if (err) throw new Error(err);
+//     console.log('Папка была создана');
+// });
+
